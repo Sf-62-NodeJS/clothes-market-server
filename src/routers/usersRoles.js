fee80c0296cf3c585faa7d93bc10a72dc8dea5fe
Velik Haznatarliev <110221236+vhazna@users.noreply.github.com>
2022-08-16 11:30:59 +0300

    Delete usersRoles.js

--- src/routers/usersRoles.js
+++
@@ -1,7 +0,0 @@
-const usersRolesRouter = require('express').Router();
-const { UsersRolesController } = require('../controllers');
-
-const usersRolesController = new UsersRolesController();
-usersRolesRouter.get('/:id', usersRolesController.getRole);
-
-module.exports = usersRolesRouter;
