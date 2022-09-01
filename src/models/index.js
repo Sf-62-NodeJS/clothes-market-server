477777bb7c1d251e77cd411c29763a13bdc47e6a
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2022-09-01 15:58:23 +0300

    Adding Sizes collection

--- src/models/index.js
+++ src/models/index.js
@@ -2,3 +2,4 @@ module.exports.User = require('./user');
 module.exports.UserStatuses = require('./userStatuses');
 module.exports.ProductStatuses = require('./productStatuses');
 module.exports.UserRoles = require('./userRoles');
+module.exports.Sizes = require('./sizes');
