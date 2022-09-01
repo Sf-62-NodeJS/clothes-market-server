6378e0e2ce78a26c28ee2ca4405f9892d2c4bf3c
Velik Haznatarliev <110221236+vhazna@users.noreply.github.com>
2022-09-01 20:44:56 +0300

    Create collection Categories with various methods in Mongoose.

--- src/models/index.js
+++ src/models/index.js
@@ -2,3 +2,4 @@ module.exports.User = require('./user');
 module.exports.UserStatuses = require('./userStatuses');
 module.exports.UserRoles = require('./userRoles');
 module.exports.Product = require('./product');
+module.exports.Categories = require('./categories');
---
+++ src/models/tests/categories.test.js
@@ -0,0 +1,67 @@
+// Just In Case
+
+const { Categories } = require('..');
+
+jest.mock('../../models', () => ({
+  Categories: class Categories {
+    static createCategory () {
+      return {
+        id: '16ad122xa2ae',
+        name: 'Jackets'
+      };
+    }
+
+    static updateCategory () {
+      return {
+        id: '16ad122xa2ae',
+        name: 'Shirts'
+      };
+    }
+
+    static deleteCategory () {
+      return true;
+    }
+
+    static getCategories () {
+      return {
+        id: '16ad122xa2ae',
+        name: 'Jackets'
+      };
+    }
+  }
+}));
+
+describe('Categories tests', function () {
+  it('should create a category of products', async () => {
+    const response = await Categories.createCategory('Jackets');
+
+    expect(response).toEqual({
+      id: '16ad122xa2ae',
+      name: 'Jackets'
+    });
+  });
+
+  it('should update a category name', async () => {
+    const response = await Categories.updateCategory('Jackets', 'Shirts');
+
+    expect(response).toEqual({
+      id: '16ad122xa2ae',
+      name: 'Shirts'
+    });
+  });
+
+  it('should delete a category', async () => {
+    const response = await Categories.deleteCategory('Shirts');
+
+    expect(response).toEqual(true);
+  });
+
+  it('should get categories', async () => {
+    const response = await Categories.getCategories('Jackets', 'Shirts');
+
+    expect(response).toEqual({
+      id: '16ad122xa2ae',
+      name: 'Jackets'
+    });
+  });
+});
