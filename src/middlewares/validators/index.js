0e672b17f9734a733f470013ae5da5751e151bd0
Asen Hodzhev <asenhodzhev@yahoo.com>
2022-09-08 19:25:06 +0300

    Fixed merge conflict

--- src/middlewares/validators/index.js
+++ src/middlewares/validators/index.js
@@ -1,6 +1,3 @@
 module.exports.createUserPayloadValidator = require('./createUserPayload');
-<<<<<<< HEAD
 module.exports.categoryPayloadValidator = require('./categoryPayload');
-=======
 module.exports.updateUserPayloadValidator = require('./updateUserPayload');
->>>>>>> 5f436f9 (Added validation for updating the user, addded tests and updating swagger)
