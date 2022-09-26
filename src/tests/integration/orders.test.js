f90d1f0b599218b06b1d50ea73c330c05f5eb166
Asen Hodzhev <asenhodzhev@yahoo.com>
2022-09-26 16:08:56 +0300

    Add passport mock to orders integration test

--- src/tests/integration/orders.test.js
+++ src/tests/integration/orders.test.js
@@ -108,6 +108,44 @@ jest.mock('mongoose', () => ({
   connect: () => {}
 }));
 
+jest.mock('passport', () => ({
+  use: jest.fn(),
+  serializeUser: jest.fn(),
+  deserializeUser: jest.fn(),
+  authenticate: () => (req, res, next) => next(),
+  session: () => (req, res, next) => next(),
+  initialize: () => (req, res, next) => {
+    req.session = {
+      passport: {
+        user: {
+          role: 'Admin'
+        }
+      }
+    };
+
+    next();
+  }
+}));
+
+jest.mock('passport-google-oauth2', () => ({
+  Strategy: class GoogleStrategy {
+    constructor (settings, verifyFunc) {
+      this.settings = settings;
+      this.verifyFunc = verifyFunc;
+    }
+  }
+}));
+
+jest.mock('passport-custom', () => ({
+  Strategy: class CustomStrategy {
+    constructor (verifyFunc) {
+      this.verifyFunc = verifyFunc;
+    }
+  }
+}));
+
+jest.mock('express-session', () => () => (req, res, next) => next());
+
 describe('Orders integration tests', function () {
   it('should create order', async () => {
     const response = await request(app)
